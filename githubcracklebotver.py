22e84d165ea6fea6042bd328d27b02e814c6dcb8
Lucas Van Beneden <lucas.vanbeneden@gmail.com>
2016-01-16 16:58:21 +0100

    fixing commit of out of date code

--- githubcracklebotver.py
+++ githubcracklebotver.py
@@ -1,14 +1,9 @@
 #! python
-<<<<<<< HEAD
 import time
 import re
 import pickle
 import praw
 import tweepy
-=======
-import praw, tweepy, time, re, pickle
-
->>>>>>> origin/master
 postedTweetsarray = []
 
 
@@ -23,13 +18,8 @@ def listentothis():
     subreddit = redditObj.get_subreddit("listentothis")
 
     for submission in subreddit.get_hot(limit=23):
-<<<<<<< HEAD
         if is_music(submission.url, submission.title):
             tweet_post(submission.title, submission.url, submission.link_flair_text)
-=======
-        if (isMusic(submission.url,submission.title)):
-            tweetPost(submission.title,submission.url,submission.link_flair_text)
->>>>>>> origin/master
 
 def music():
     """Search for posts in music subreddit"""
@@ -37,11 +27,7 @@ def music():
     postFlair = ''
 
     for submission in subreddit.get_hot(limit=20):
-<<<<<<< HEAD
         if is_music(submission.url, submission.title):
-=======
-        if (isMusic(submission.url,submission.title)):
->>>>>>> origin/master
             if ('music streaming' in submission.link_flair_text) or ('stream' in submission.link_flair_text):
                 postFlair = submission.title
                 postFlair = postFlair[postFlair.find("[")+1:postFlair.find("]")]
@@ -51,11 +37,7 @@ def indieheads():
     subreddit = redditObj.get_subreddit("indieheads")
 
     for submission in subreddit.get_hot(limit=20):
-<<<<<<< HEAD
         if is_music(submission.url, submission.title):
-=======
-        if (isMusic(submission.url,submission.title)):
->>>>>>> origin/master
             if('Fresh' in submission.title) or('fresh' in submission.title)or('FRESH' in submission.title)or('ORIGINAL' in submission.title)or('Original' in submission.title) or ('original' in submission.title):
                 tweet_post(submission.title, submission.url, "Indie")
 def hiphopheads():
@@ -63,11 +45,7 @@ def hiphopheads():
     subreddit = redditObj.get_subreddit("hiphopheads")
 
     for submission in subreddit.get_hot(limit=20):
-<<<<<<< HEAD
         if is_music(submission.url, submission.title):
-=======
-        if (isMusic(submission.url,submission.title)):
->>>>>>> origin/master
             if('Fresh' in submission.title) or('fresh' in submission.title)or('FRESH' in submission.title)or('ORIGINAL' in submission.title)or('Original' in submission.title) or ('original' in submission.title):
                 tweet_post(submission.title, submission.url, 'hiphop')
 def electronicmusic():
@@ -76,14 +54,8 @@ def electronicmusic():
     postFlair = 'electronic'
 
     for submission in subreddit.get_hot(limit=20):
-<<<<<<< HEAD
         if is_music(submission.url, submission.title):
             tweet_post(submission.title, submission.url, postFlair)
-=======
-        if (isMusic(submission.url,submission.title)):
-            tweetPost(submission.title,submission.url,PostFlair)
-
->>>>>>> origin/master
 #twitter bot function
 def tweet_post(title, url, flair):
     """Post found tweets to twitter"""
@@ -114,10 +86,6 @@ def tweet_post(title, url, flair):
         nonDuplicateFlag = False
     
     if (nonDuplicateFlag):
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
         try:
             api.update_status(tweet)#add tweet to timeline
         except:
@@ -145,18 +113,11 @@ def tweet_cleanup(returnTweet):
     returnTweet = re.sub('#[\w]{3}[-][\w]{3}', '#hiphop', returnTweet)
     #turn hip-hop into hiphop for hashtag handling
     return returnTweet
-<<<<<<< HEAD
 def is_music(url, title):
     """check if reddit post is playlist"""
     if ('youtube.com' in url) or ('soundcloud.com' in url) or ('spotify.com' in url) or ('bandcamp.com' in url):
         if('playlist' in title) or ('Playlist' in title):
         #check if playlist link or not. If playlist detected the link is discarded
-=======
-
-def isMusic(URL, title):
-    if ('youtube.com' in URL) or ('soundcloud.com' in URL) or ('spotify.com' in URL) or ('bandcamp.com' in URL):
-        if('playlist' in title) or ( 'Playlist' in title):#check if playlist link or not. If playlist detected the link is discarded
->>>>>>> origin/master
             return False
         else:
             return True
